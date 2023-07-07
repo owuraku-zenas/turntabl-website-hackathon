639b5464c22f1d6eb61f15db3ee0482795922c45
deborah-asamoah <deborah.asamoah@turntabl.io>
2023-07-07 21:43:09 +0000

    added the header component

--- src/app/our-approach/page.tsx
+++ src/app/our-approach/page.tsx
@@ -1,10 +1,18 @@
+import Header from '@/components/Header'
 import React from 'react'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div>Our Approach</div>
+    <div className='min-h-[100vh]'>
+      <Header 
+      title='Our Approach'
+      description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      image='contact-header.jpg'
+      />
+      
+      Our Approach</div>
   )
 }
 
