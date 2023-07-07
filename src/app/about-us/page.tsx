e6ac24d7b3972b3fbeed71492753c78c29456657
deborah-asamoah <deborah.asamoah@turntabl.io>
2023-07-07 21:40:37 +0000

    added the headed to the about-us page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,10 +1,18 @@
+import Header from '@/components/Header'
 import React from 'react'
 
 type Props = {}
+import mypic from '../../../public/images/contact-header.jpg'
 
 const page = (props: Props) => {
   return (
-    <div>About Us</div>
+    <div className='min-h-[100vh]'>
+      <Header 
+      title='About Us'
+      description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      image='contact-header.jpg'
+      />
+      About Us</div>
   )
 }
 
